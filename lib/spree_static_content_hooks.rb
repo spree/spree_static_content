f9a2f5e135031e495f4cdb766381828bf0282f14
Pete Campbell <pete@sumirolabs.com>
2011-08-14 11:29:36 -0400

    Added Deface requirement so the generator would work

--- lib/spree_static_content_hooks.rb
+++ lib/spree_static_content_hooks.rb
@@ -1,3 +1,5 @@
+require 'deface'
+
 Deface::Override.new(:virtual_path => "layouts/admin",
                      :name => "static_content_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
