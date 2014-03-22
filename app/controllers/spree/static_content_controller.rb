60c55a8f5cf15b97372bbdc4aa08d074fe851ad4
Rein Aris <info@reinaris.nl>
2014-03-22 21:02:38 +0100

    Deleted unused method

--- app/controllers/spree/static_content_controller.rb
+++ app/controllers/spree/static_content_controller.rb
@@ -3,7 +3,7 @@ class Spree::StaticContentController < Spree::StoreController
 
   helper 'spree/products'
   helper 'spree/pages'
-  
+
   layout :determine_layout
 
   def show
@@ -18,10 +18,5 @@ class Spree::StaticContentController < Spree::StoreController
     else
       Spree::Config.layout
     end
-  end
-
-  # TODO: Is this method used?
-  def accurate_title
-    @page && @page.meta_title.present? ? @page.meta_title : @page.title
   end
 end
