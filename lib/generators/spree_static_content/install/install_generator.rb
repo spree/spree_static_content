f70bc497e6f63f162cbbd83ceed8ced1585dd213
18augst <18augst@gmail.com>
2014-11-01 17:34:57 +0300

    tinymce integrated

--- lib/generators/spree_static_content/install/install_generator.rb
+++ lib/generators/spree_static_content/install/install_generator.rb
@@ -7,6 +7,25 @@ module SpreeStaticContent
         inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_static_content\n", before: /\*\//, verbose: true
       end
 
+      def add_backend_javascripts
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.js', "//= require spree/backend/spree_static_content"
+      end
+
+      def add_tinymce_configuration
+        inject_into_file 'config/tinymce.yml' do <<-YML
+spree_static_content:
+  selector: textarea.tinymce-spree-static-content
+  toolbar:
+    - styleselect | bold italic | undo redo
+    - image | link
+  plugins:
+    - table
+    - code
+    - image
+    - link
+YML
+      end
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_static_content'
       end
