4d620e8886a490cd0573e41f729c78ec473e45fc
Tomas Srna <tomas@srna.sk>
2015-08-29 22:15:01 +0200

    Ordering

--- app/models/spree/page.rb
+++ app/models/spree/page.rb
@@ -41,12 +41,12 @@ class Spree::Page < ActiveRecord::Base
     # Ensure that all slugs start with a slash.
     slug.prepend('/') if not_using_foreign_link? && !slug.start_with?('/')
     return if new_record?
-    return unless (prev_position = Spree::Page.find(id).position)
-    if prev_position > position
-      Spree::Page.where('? <= position and position < ?', position, prev_position).update_all('position = position + 1')
-    elsif prev_position < position
-      Spree::Page.where('? < position and position <= ?', prev_position, position).update_all('position = position - 1')
-    end
+    # return unless (prev_position = Spree::Page.find(id).position)
+    # if prev_position > position
+    #   Spree::Page.where('? <= position and position < ?', position, prev_position).update_all('position = position + 1')
+    # elsif prev_position < position
+    #   Spree::Page.where('? < position and position <= ?', prev_position, position).update_all('position = position - 1')
+    # end
   end
 
   def not_using_foreign_link?
